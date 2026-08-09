2823a1fcd68b3f4f3ec62b5355c54b13758a86d2
VijayRamisetty <ramisetty.v@outlook.com>
2026-08-09 23:00:15 +0530

    tp - palindrome string

---
+++ part_a/tp/01_valid_palindrome.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        l, r = 0, len(s)-1
+        while l<r:
+            while l<r and not s[l].isalnum():
+                l+=1
+            while l<r and not s[r].isalnum():
+                r-=1
+            
+            if s[l].lower() != s[r].lower():
+                return False
+            l+=1
+            r-=1
+        return True
+
+sol = Solution()
+assert (received:=sol.isPalindrome("A man, a plan, a canal: Panama")) == True, f'Failed {received=}'
+assert (received:=sol.isPalindrome("race a car")) == False, f'Failed {received=}'
+assert (received:=sol.isPalindrome(" ")) == True, f'Failed {received=}'
