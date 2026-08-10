0c3e390370a28f0c0139a7fe28dd658f833379a7
VijayRamisetty <ramisetty.v@outlook.com>
2026-08-10 20:48:22 +0530

    monotonic stack

---
+++ part_a/monotonic_stack/01_next_greater_element.py
@@ -0,0 +1,16 @@
+from typing import List
+class Solution:
+    def nextGreater(self, nums:List) -> List:
+        stack = []
+        res = [-1] * len(nums)
+        for i, n in enumerate(nums):
+            print(stack)
+            while stack and n > nums[stack[-1][0]]:  # current > top_of_stack
+                 popped_index, val = stack.pop()
+                 res[popped_index] = n 
+            stack.append((i,n))
+        return res
+sol = Solution()
+assert (received:=sol.nextGreater([73,74,75,71,69,72,76,73])) == [74, 75, 76,72,72,76, -1 , -1], f'Failed {received=}'
+print('-'*10)
+assert (received:=sol.nextGreater([1, 3, 2, 4])) == [3, 4, 4, -1], f'Failed {received=}'
